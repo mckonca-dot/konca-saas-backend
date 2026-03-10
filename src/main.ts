25c85964cb3cb1132e2aa55811debbbe91a8b361
Muhammet KONCA <mckonca@gmail.com>
2026-03-11 00:57:09 +0300

    feat: logo ekleme hatası düzeltildi

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express'; // 🚀 Express yardımcılarını ekledik
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 🛡️ SİBER GÜVENLİK KALKANI (CORS) - Sadece izinli siteler girebilir!
+  // 🔥 SİHİRLİ DOKUNUŞ: Sunucunun veri taşıma kapasitesini 10MB'a çıkarıyoruz
+  // Bu ayar sayesinde logolar yüklenirken oluşan "413 Payload Too Large" hatası çözülür.
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true, limit: '10mb' }));
+
+  // 🛡️ SİBER GÜVENLİK KALKANI (CORS) - Mevcut ayarlarını harfiyen koruyoruz
   app.enableCors({
     origin: [
-      'http://localhost:3000', // Senin kendi bilgisayarın (geliştirme yaparken lazım)
-      'https://randevu-saas-frontend-ghqr.vercel.app', // 👈 KENDİ VERCEL LİNKİNİ BURAYA YAPIŞTIR
-      // İleride .com domaini alınca onu da buraya ekleyeceğiz.
+      'http://localhost:3000', 
+      'https://randevu-saas-frontend-ghqr.vercel.app', 
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true, // Oturum açma (token) işlemleri için şart
+    credentials: true, 
   });
 
   // Render'ın atadığı portu veya 3001'i dinle
